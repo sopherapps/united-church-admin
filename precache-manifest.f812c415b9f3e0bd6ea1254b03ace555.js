e613706a602552cf191dc33bd82449c3f5a5bb2b
Martin Ahindura <martinahindura@gmail.com>
2019-08-18 07:09:04 +0300

    Updates

---
+++ precache-manifest.f812c415b9f3e0bd6ea1254b03ace555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25e2f70829caced2928f8c395119505",
+    "url": "/united-church-admin/index.html"
+  },
+  {
+    "revision": "35096221608c4198f487",
+    "url": "/united-church-admin/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5a1ade993df64fb288c7",
+    "url": "/united-church-admin/static/js/2.b61a78cb.chunk.js"
+  },
+  {
+    "revision": "35096221608c4198f487",
+    "url": "/united-church-admin/static/js/main.20ea019e.chunk.js"
+  },
+  {
+    "revision": "93f6b9a012e1cffe2ca8",
+    "url": "/united-church-admin/static/js/runtime~main.adc49a2c.js"
+  }
+]);
